d788db5247e43ed3a4ba1a0e6f8aba2ddeaece17
Martin Møller <mmj@dbc.dk>
2014-02-11 15:59:55 +0100

    Removing search controls if no search results are shown

--- js/bibdk_search_controls.js
+++ js/bibdk_search_controls.js
@@ -1,33 +1,48 @@
 (function($) {
+  var BibdkSearchControls = {};
 
-  Drupal.behaviors.bibdkSearchControlsValues = {
-    attach: function(context, settings) {
-      $('.bibdk-search-controls-form').change(function() {
-        var value = "";
-        var idHidden = $(this).attr('data-control-name');
+  BibdkSearchControls.setCheckboxes = function() {
+    $('.bibdk-search-controls-form').change(function() {
+      var value = "";
+      var idHidden = $(this).attr('data-control-name');
 
-        if ( selectValue = $(this).find(".form-type-select select").val() )  {
-          value = selectValue;
-        }
+      if(selectValue = $(this).find(".form-type-select select").val()) {
+        value = selectValue;
+      }
 
-        if ( radiosValue = $(this).find(".form-type-radios input:checked").val() )  {
-          value = radiosValue;
-        }
+      if(radiosValue = $(this).find(".form-type-radios input:checked").val()) {
+        value = radiosValue;
+      }
 
-        $(this).find(".form-type-checkboxes .form-item input:checked").each(function (i) {
-          if ( $(this).val() ) {
-            if ( value ) {
-              value += "|";
-            }
-            value += $(this).val();
+      $(this).find(".form-type-checkboxes .form-item input:checked").each(function(i) {
+        if($(this).val()) {
+          if(value) {
+            value += "|";
           }
-        })
-
-        $("#" + idHidden).val(value);
-
+          value += $(this).val();
+        }
       });
+      $("#" + idHidden).val(value);
+    });
+  };
+
+  BibdkSearchControls.hideSearchControls = function(context) {
+    if(Drupal.settings.ting_openformat == undefined){
+      $('.works-controls', context).css({
+        display: 'none'
+      });
+
+      $('.panel-left', context).css({
+        display: 'none'
+      })
     }
   };
 
-} (jQuery));
+  Drupal.behaviors.bibdkSearchControlsValues = {
+    attach: function(context, settings) {
+      BibdkSearchControls.setCheckboxes();
+      BibdkSearchControls.hideSearchControls(context);
+    }
+  };
+}(jQuery));
 
